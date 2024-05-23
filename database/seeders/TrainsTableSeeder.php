494aa0f047639c2fb9cd848f54f65f543efe7e5f
Frankosviz <geom.francescopaolo@gmail.com>
2024-05-23 16:20:45 +0200

    laravel-migration-seeder : seeded db

--- database/seeders/TrainsTableSeeder.php
+++ database/seeders/TrainsTableSeeder.php
@@ -14,8 +14,21 @@ class TrainsTableSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
+    public function run(Faker $faker)
     {
-        
+        for ($i = 0; $i < 20; $i++) {
+        $new_train = new Train();
+        $new_train->azienda = $faker->company();
+        $new_train->stazione_di_partenza = $faker->city();
+        $new_train->stazione_di_arrivo = $faker->city();
+        $new_train->orario_di_partenza = $faker->time();
+        $new_train->orario_di_arrivo = $faker->time();
+        $new_train->codice_treno = $faker->bothify('??-##-??');
+        $new_train->numero_carrozze = $faker->numberBetween(0, 15);
+        $new_train->in_orario = $faker->boolean();
+        $new_train->cancellato = $faker->boolean();
+        $new_train->data = $faker->date();
+        $new_train->save();
     }
+}
 }
